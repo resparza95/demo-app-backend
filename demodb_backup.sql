b48428642dfa9fbba45bdd3a801a43bcce349956
Richard Esparza <resparza@plugthem.social>
2025-05-01 14:42:54 -0500

    refactor: updated db backup file

--- demodb_backup.sql
+++ demodb_backup.sql
@@ -1,5 +1,5 @@
 -- Host: 127.0.0.1
--- Generation Time: May 01, 2025 at 08:19 AM
+-- Generation Time: May 01, 2025 at 09:39 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -29,8 +29,20 @@ CREATE TABLE `entries` (
   `title` varchar(255) NOT NULL,
   `points` int(11) DEFAULT NULL,
   `commentCount` int(11) DEFAULT NULL,
-  `createdAt` date DEFAULT NULL,
-  `updatedAt` date DEFAULT NULL
+  `createdAt` timestamp NULL DEFAULT NULL,
+  `updatedAt` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Table structure for table `usage_data`
+--
+
+CREATE TABLE `usage_data` (
+  `id` bigint(20) NOT NULL,
+  `appliedFilter` varchar(255) NOT NULL,
+  `appliedOrder` varchar(255) NOT NULL,
+  `createdAt` timestamp NULL DEFAULT NULL,
+  `updatedAt` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -44,6 +56,12 @@ ALTER TABLE `entries`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `usage_data`
+--
+ALTER TABLE `usage_data`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -51,7 +69,13 @@ ALTER TABLE `entries`
 -- AUTO_INCREMENT for table `entries`
 --
 ALTER TABLE `entries`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=421;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
+
+--
+-- AUTO_INCREMENT for table `usage_data`
+--
+ALTER TABLE `usage_data`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
